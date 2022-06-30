59f2bca9973664cb050a75647dcee37f4af85f45
Ian Halcomb <halcombian@gmail.com>
2022-06-30 01:02:32 -0400

    Give Add Book button and window console functionality

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+let myLibrary = [];
+
 function Book(title, author, pages, read) {
 	this.title = title;
 	this.author = author;
@@ -8,11 +10,58 @@ function Book(title, author, pages, read) {
 	};
 }
 
-const theHobbit = new Book(
-	"The Hobbit",
-	"J.R.R. Tolkien",
-	"295 pages",
-	"not read yet"
-);
+let readUnread;
 
-console.log(theHobbit.info());
+const readBtn = document.querySelector("#read-btn");
+readBtn.addEventListener("click", () => {
+	return (readUnread = "read");
+});
+
+const unreadBtn = document.querySelector("#unread-btn");
+unreadBtn.addEventListener("click", () => {
+	return (readUnread = "unread");
+});
+
+let i = 0;
+
+const titleInput = document.querySelector("[name=title-input]");
+const authorInput = document.querySelector("[name=author-input]");
+const pagesInput = document.querySelector("[name=pages-input]");
+
+function addBookToLibrary() {
+	i = new Book(
+		titleInput.value,
+		authorInput.value,
+		pagesInput.value,
+		readUnread
+	);
+	myLibrary.push(i);
+	console.log(i.info());
+	i++;
+}
+
+function clearAddBookWindow() {
+	titleInput.value = "";
+	authorInput.value = "";
+	pagesInput.value = "";
+	readUnread = "";
+	addBookWindow.style.visibility = "hidden";
+}
+
+const submitBtn = document.querySelector(".submit-btn");
+submitBtn.addEventListener("click", () => {
+	addBookToLibrary();
+	clearAddBookWindow();
+});
+
+const addBookWindow = document.querySelector(".add-book-window"); //Add Book window
+
+const addBookBtn = document.querySelector(".add-book-btn"); //Add Book button
+addBookBtn.addEventListener("click", () => {
+	addBookWindow.style.visibility = "visible";
+});
+
+const closeBtn = document.querySelector(".close-btn"); //Close window button
+closeBtn.addEventListener("click", () => {
+	clearAddBookWindow();
+});
